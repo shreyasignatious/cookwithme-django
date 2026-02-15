93a8c5bef79758e1358ba74fe68076ff831caf5f
shreyas ignatious <shreyas01reddy@gmail.com>
2026-02-15 13:08:56 +0000

    Switched database to SQLite for deployment

--- cookwithme/settings.py
+++ cookwithme/settings.py
@@ -76,16 +76,24 @@ WSGI_APPLICATION = 'cookwithme.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'project1',#databasename
+#         'USER':'postgres',#user name in postgres
+#         'PASSWORD':'root',
+#         'HOST':'localhost',
+#         'PORT':'5432'
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'project1',#databasename
-        'USER':'postgres',#user name in postgres
-        'PASSWORD':'root',
-        'HOST':'localhost',
-        'PORT':'5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
